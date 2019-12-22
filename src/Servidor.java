5b58688a0b4c54015ba9385ae718e09665a59088
Caio Fernando Soares Deambrosio <caio.deambrosio@gmail.com>
2019-12-22 15:01:38 -0300

    Improvement - adding license and improve server experience

--- src/Servidor.java
+++ src/Servidor.java
@@ -3,6 +3,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Servidor {
 
 	static ArrayList<Socket> clientes;
@@ -13,6 +15,10 @@ public class Servidor {
 
 		server = new ServerSocket(7000);
 		clientes = new ArrayList<Socket>();
+
+		Object[] options = { "OK" };
+		JOptionPane.showOptionDialog(null, "Servidor inicializado com sucesso! \nClique em OK para que seus clientes possam se conectar.", "Sucesso", JOptionPane.PLAIN_MESSAGE,
+				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
 		while (true) {
 			connection = server.accept();
